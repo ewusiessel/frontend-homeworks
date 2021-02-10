d2e84d8dc72809cb8c9f25f856e21c4aeee9073d
Ekow <ewusiessel@hotmail.com>
2021-02-10 15:52:26 +0000

    submit homework

---
+++ m1-d8/index.js
@@ -0,0 +1,17 @@
+ /* EXERCISE 5: 
+          Add a new task to the list.
+          Suggestion:
+          - Use document.getElementById to get the UL item and the input text
+          - Use the document.createElement to create the new List Item
+          - Append the child to the UL
+      */
+
+      function addTask(){
+          const newTask = document.getElementById('myTaskList')
+          console.log(newTask.innerHTML)
+          newTask,innerText = " "
+      }
+
+      const addedNewTask = document.createElement('div')
+
+      
