e0beb6a571803f0d10b580e795b4b72cfe897c9d
Ekow <ewusiessel@hotmail.com>
2021-02-12 15:52:55 +0000

    submit homework

---
+++ m1-d10/index.js
@@ -0,0 +1,13 @@
+const x = "John";
+const y = "Doe";
+
+console.log(x + "<" + ">" + y);
+
+anObject = {
+  name: "John",
+  surname: "Parker",
+  email: "johnparker@email.com",
+};
+
+delete anObject.email;
+console.log(anObject);
