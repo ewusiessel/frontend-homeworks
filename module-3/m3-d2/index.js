1dc7a699389e2a831eab4efc33dfb6ef28eeeddf
Ekow <ewusiessel@gmail.com>
2021-04-14 03:56:37 +0100

    refactor homework to fetch api

--- module-3/m3-d2/index.js
+++ module-3/m3-d2/index.js
@@ -1,32 +1,34 @@
-// fetch api
-function listAlbums() {
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "e231654c7bmshc8457f88c6def78p1ccaa2jsnf9f1cf1ca0b5",
-            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        },
-    })
-        .then((response) => {
-            console.log(response.json());
-        })
-        .then(books => {
-            const row = document.querySelector(".row")
-            row.innerText = ""
+const btn = document.getElementById("btn")
+btn.addEventListener("click", getAlbums)
+const div = document.getElementById("cardDiv")
 
-            for (let i = 0; i < listAlbums.length; i++) {
-                const album = album[i]
-                const albumDiv = document.createElement("div")
-                albumDiv.classList.add("col-12")
-                albumDiv.classList.add("col-md-3")
-                const img = document.createElement("img")
-                img.src = book.img
-                img.classList.add("img-fluid")
-                albumDiv.appendChild(img)
-                row.appendChild(albumDiv)
-            }
-      
-            // .catch((err) => {
-            //   console.error(err);
-            // });
-        }
+
+// fetch api
+function getAlbums() {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "e231654c7bmshc8457f88c6def78p1ccaa2jsnf9f1cf1ca0b5",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json()) 
+      .then((data) => {
+         
+        for (let i = 0; i < data.data.length; i++){
+            div.innerHTML += `
+            <div class="card col-3 m-1 mx-auto">
+                <img class="card-img-top" src="${data.album}">
+                <div class="card-body">
+                <h5 class="card-title">${data.title}</h5>
+                <p class="card-text">${data.id}</p>
+                </div>
+            </div>
+          `
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+    });
+};
